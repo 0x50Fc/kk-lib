2663b3730e00eef7196112fa2e184f168bbcfc41
hailong11 <hailong11@staff.weibo.com>
2018-09-11 14:45:02 +0800

    update

--- imagequant/cflags_windows.go
+++ imagequant/cflags_windows.go
@@ -1,9 +1,9 @@
 //+build windows
 
-package quant
+package imagequant
 
 /*
-#cgo CFLAGS: -O3 -fno-math-errno -fopenmp -funroll-loops -fomit-frame-pointer -Wall -Wno-attributes -std=c99 -DNDEBUG -DUSE_SSE=1 -msse -fexcess-precision=fast
-#cgo LDFLAGS: -fopenmp -static
+#cgo CFLAGS: -O3 -fomit-frame-pointer -Wall -Wno-attributes -std=c99 -DNDEBUG -DUSE_SSE=1 -msse -fexcess-precision=fast
+#cgo LDFLAGS: -static
 */
 import "C"
