4e426e51f08bcfe100c76e509f5e925cb0d13d70
hailong11 <hailong11@staff.weibo.com>
2018-09-12 15:26:01 +0800

    rm pngquant

--- imagequant/cflags_linux.go
+++
@@ -1,9 +0,0 @@
-//+build !windows
-
-package imagequant
-
-/*
-#cgo CFLAGS: -O3  -Wunknown-pragmas -fomit-frame-pointer -Wall -Wno-attributes -std=c99 -DNDEBUG -DUSE_SSE=1 -msse
-#cgo LDFLAGS: -lm
-*/
-import "C"
--- imagequant/cflags_windows.go
+++
@@ -1,9 +0,0 @@
-//+build windows
-
-package imagequant
-
-/*
-#cgo CFLAGS: -O3 -fomit-frame-pointer -Wall -Wno-attributes -std=c99 -DNDEBUG -DUSE_SSE=1 -msse -fexcess-precision=fast
-#cgo LDFLAGS: -static
-*/
-import "C"
